7fec8964c0b4843f09c5fe169946004d9f8f2344
Konrad Kusiak <kkusiak@modular.com>
2025-11-02 16:06:06 +0000

    removed unused tmux scripts

--- config/tmux/hooks/session-create.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-session_name=$(tmux display-message -p '#S')
-session_layout=~/.local/share/tmux/layouts/session-$session_name
-if [ -f "$session_layout" ]; then
-  $session_layout
-else
-  tmux rename-window home
-fi
--- config/tmux/visor.zsh
+++
@@ -1,6 +0,0 @@
-if ! tmux list-sessions -F '#{session_name}' | grep 'visor' > /dev/null; then
-  tmux new-session -d -s visor
-  tmux rename-window -t visor:0 home
-fi
-tmux attach-session -t visor
-exit
