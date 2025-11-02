1df162b7d096f4888b5d2af3852159a63fc6bb98
Konrad Kusiak <konrad.kusiak@codeplay.com>
2025-11-02 19:47:34 +0000

    Removed redundant submodules

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,20 @@
 return {
-  "folke/tokyonight.nvim",
-  -- opts = {
-  --   transparent = true,
-  --   styles = {
-  -- sidebars = "transparent",
-  -- floats = "transparent",
-  -- },
-  -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "frappe", -- latte, frappe, macchiato, mocha
+      -- transparent = true,
+      -- styles = {
+      --   sidebars = "transparent",
+      --   floats = "transparent",
+      -- },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
 }
